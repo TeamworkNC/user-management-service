0291bd49392b44da41e2342e3107a6cd17440eee
weldnor <kolesov.anton.s@gmail.com>
2021-02-24 19:37:29 +0300

    Refactor project, remove some classes and packages

--- src/main/java/com/moviesandchill/usermanagementservice/Application.java
+++ src/main/java/com/moviesandchill/usermanagementservice/Application.java
@@ -1,4 +1,4 @@
-package com.moviesandchill.usermanagmentservice;
+package com.moviesandchill.usermanagementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
