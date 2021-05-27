230b6b5cfb00f2d577d06bbf8f216556f1c36f4c
weldnor <kolesov.anton.s@gmail.com>
2021-05-27 23:15:38 +0300

    Fix heroku config

--- src/main/java/com/moviesandchill/usermanagementservice/config/ApplicationConfig.java
+++ src/main/java/com/moviesandchill/usermanagementservice/config/ApplicationConfig.java
@@ -41,7 +41,7 @@ public class ApplicationConfig {
         CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(usernameElastic, passwordElastic));
         RestHighLevelClient restClient = new RestHighLevelClient(
-                RestClient.builder(new HttpHost(hostElastic, portElastic, "http"))
+                RestClient.builder(new HttpHost(hostElastic, portElastic, "https"))
                         .setHttpClientConfigCallback(httpAsyncClientBuilder ->
                                 httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
                                         .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())));
